6e98498894a215add2646516967620014b0aea81
Kaloyan Maksimov <k.maksimov90@gmail.com>
2024-04-23 14:16:15 +0300

    Add API Skeleton

---
+++ API/Data/DatingAppContext.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DatingAppContext : DbContext
+{
+    public DatingAppContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
